597c0e5d39e06df1d3e34601173f300e63a12945
Toni Blankenburg <toniblankenburg@gmx.net>
2026-06-07 12:36:40 +0200

    Feedback implemented

--- src/utils/faq.ts
+++ src/utils/faq.ts
@@ -2,14 +2,10 @@
 import {readFileSync} from 'fs';
 
 function loadFaqChunks(): string[] {
-    return Faq.split('##').map((chunk:string) => chunk.trim()).filter((chunk: string) => chunk.length > 0);
-}
+    const faq = readFileSync(__dirname + '/../data/faq.md', 'utf-8');
 
-function loadFaq(): string {
-    return readFileSync('./src/data/faq.md', 'utf-8');
+    return faq.split('##').map((chunk:string) => chunk.trim()).filter((chunk: string) => chunk.length > 0);
 }
-
-const Faq = loadFaq();
 
 export function retrieveChunks(userMessage: string, topN: number = 3): string[] {
     const words = userMessage.toLowerCase().split(/\s+/);
